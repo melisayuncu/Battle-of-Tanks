066de4cba6ce3c65d086f118f5c57268c0160671
melisayuncu <99989807+melisayuncu@users.noreply.github.com>
2024-01-05 15:01:58 +0300

    Update TankShooter.cs
    comment lines

--- Assets/Scripts/TankShooter.cs
+++ Assets/Scripts/TankShooter.cs
@@ -4,39 +4,42 @@ using UnityEngine;
 
 public class TankShooter : MonoBehaviour
 {
-   // public Transform firePoint;
+    // public Transform firePoint;
+    // Prefab of the bullet to be shot
     public GameObject bulletPrefab;
+
+    // Force applied to the bullet when shot
     public float bulletForce = 25.0f;
-    
-    //private Transform enemy;
-    void Start(){
 
-    }
+    // Update is called once per frame
     void Update()
-{
-    // Check for the left mouse button or the Space key
-    if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump"))
     {
-        Shoot();
+        // Check for the left mouse button or the Space key
+        if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump"))
+        {
+            // Call the Shoot method when the fire button is pressed
+            Shoot();
+        }
     }
-}
 
-
-    
-
+    // instantiate and shoot a bullet
     void Shoot()
     {
-        
+        // Get the spawn position as the tank's current position
         Vector3 spawnPosition = transform.position;
-       //Quaternion spawnRotation = Quaternion.identity;
+
+        // Get the spawn rotation as the tank's current rotation
         Quaternion spawnRotation = transform.rotation;
+
+        // Get the local forward direction of the tank and apply bullet force
         Vector3 localXDirection = transform.TransformDirection(Vector3.forward);
         Vector3 velocity = localXDirection * bulletForce;
 
+        // Instantiate a bullet at the calculated position and rotation
         GameObject bullet = Instantiate(bulletPrefab, spawnPosition, spawnRotation);
+
+        // Get the Rigidbody component of the bullet and set its velocity
         Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
         bulletRb.velocity = velocity;
-
     }
-    
 }
