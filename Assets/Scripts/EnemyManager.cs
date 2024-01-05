1f1064fced6a3e4b94ee50cb618181296e62e4ba
melisayuncu <99989807+melisayuncu@users.noreply.github.com>
2024-01-05 14:52:18 +0300

    Update EnemyManager.cs
    comment lines

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    // Array of spawn points where enemies will be instantiated
     public Transform[] m_SpawnPoints;
-    public GameObject m_EnemyPrefab;
-    public int maxEnemies = 10; // Maximum number of enemies to spawn
 
+    // Reference to the enemy prefab
+    public GameObject m_EnemyPrefab;
+
+    // Maximum number of enemies to spawn
+    public int maxEnemies = 10;
+
+    // Current count of spawned enemies
     private int currentEnemyCount = 0;
 
     void Start()
     {
+        // Spawn initial set of enemies when the game starts
         SpawnAllEnemies();
     }
 
     void OnEnable()
     {
+        // Subscribe the SpawnSingleTank method to the onEnemyKilled event
         EnemyHealth.onEnemyKilled += SpawnSingleTank;
+
+        // Subscribe a lambda expression to the onEnemyKilled event to decrement the enemy count
         EnemyHealth.onEnemyKilled += () => currentEnemyCount--;
     }
 
+    // Method to spawn all enemies at the start of the game
     void SpawnAllEnemies()
     {
+        // Check if the enemy prefab is assigned
         if (m_EnemyPrefab != null)
         {
+            // Instantiate enemies at each spawn point
             foreach (Transform spawnPoint in m_SpawnPoints)
             {
                 Instantiate(m_EnemyPrefab, spawnPoint.position, Quaternion.identity);
@@ -40,9 +53,12 @@ public class EnemyManager : MonoBehaviour
         {
             // Choose a random spawn point
             Transform spawnPoint = m_SpawnPoints[Random.Range(0, m_SpawnPoints.Length)];
+
+            // Instantiate the enemy prefab at the chosen spawn point
             Instantiate(m_EnemyPrefab, spawnPoint.position, Quaternion.identity);
+
+            // Increment the current enemy count
             currentEnemyCount++;
         }
-
     }
 }
