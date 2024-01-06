30a615fd1da1560511e5f424cdc1e3d690686f8e
CodeRunner24 <124782697+CodeRunner24@users.noreply.github.com>
2024-01-06 15:04:09 +0300

    Update Spin.cs

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
+    // Speed of the spin
     public float spinSpeed = 90.0f;
        
     // Update is called once per frame
     void Update()
-    {
+    {    
+        // if the button "E" is pressed
         if(Input.GetKey(KeyCode.E)){      
               transform.Rotate(Vector3.up, spinSpeed*Time.deltaTime);
-}
-    
-    
+        }
+        // if the button "Q" is pressed
         if(Input.GetKey(KeyCode.Q)){   
                  transform.Rotate(Vector3.up, -spinSpeed*Time.deltaTime);
-}
-    
-}
+        }
+    }
 }
